0dda15fb4b0bcccc0112aef6a79cecc45e2f84ef
Vladimir Odesskij <odesskij1992@gmail.com>
2016-01-07 20:31:48 +0200

    bootstrap

--- controllers/views/index.js
+++ controllers/views/index.js
@@ -3,7 +3,12 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
+  res.render('dashboard/index', {
+    title: 'Express'
+  });
+});
+router.get('/login', function (req, res, next) {
+  res.render('dashboard/login', {
     title: 'Express'
   });
 });
