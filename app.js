665f951d0795520ada7c94604cb6b5e18cac19e5
tomfun <tomfun1990@gmail.com>
2016-01-07 20:24:18 +0200

    structure

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-
 import express from 'express';
-
-//import express  from 'express'
 import path from 'path';
-import favicon from 'serve-favicon';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
+import initialize from './controllers/initialize';
 import log4js from 'log4js';
 
-import routes from './routes/index';
 
-var app = express();
+let app = express();
 
 log4js.configure({
   appenders: [
@@ -19,7 +13,7 @@ log4js.configure({
   ]
 });
 
-var logger = log4js.getLogger('console');
+let logger = log4js.getLogger('console');
 logger.setLevel(log4js.levels.INFO);
 
 app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO, format: ':method :status [:response-time ms] :url ' }));
@@ -28,44 +22,6 @@ app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO, format: ':meth
 app.set('views', path.join(__dirname, 'frontend', 'templates'));
 app.set('view engine', 'twig');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+initialize(app);
 
 export default app;
---
+++ controllers/middlewares.js
@@ -0,0 +1,48 @@
+import path from 'path';
+import favicon from 'serve-favicon';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import express from 'express';
+
+export function preRoutes (app) {
+  app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+};
+
+export default function (app) {
+// catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error:   err
+      });
+    });
+  }
+
+// production error handler
+// no stacktraces leaked to user
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error:   {}
+    });
+  });
+};
+
