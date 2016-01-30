02cf8ec316915b454d737568d7983fc947a99308
tomfun <tomfun1990@gmail.com>
2016-01-30 15:47:13 +0200

    init tests

---
+++ test/0-tripple.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//var test = require('unit.js');
+//var expect = require('expect');
+var should = require('should');
+var _ = require('lodash');
+
+describe('date module', function () {
+  //var tripleBad = require('../app/helpers/elastic/tripleBad.js');
+  //it('validate simple', function () {
+  //  timeValidator.validate("12:53").should.be.Object();
+  //});
+  //it('safeValidate fail', function () {
+  //  timeValidator.safeValidate("48:53").should.be.equal(false);
+  //});
+  //it('validate tz fail', function () {
+  //  should.throws(function () {
+  //    timeValidator.validate("12:53-12:00");
+  //  });
+  //});
+  //it('parse empty tz', function () {
+  //  obj.utcTimestamp.should.be.NaN();
+  //});
+});
